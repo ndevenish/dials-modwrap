2c92572be01cbe3307fac911cd1a8a604ab47c77
Nicholas Devenish <ndevenish@gmail.com>
2017-07-28 11:41:49 +0100

    Add a generated cmake structure

---
+++ cmake/Modules/CCP4IOGeneration.cmake
@@ -0,0 +1,39 @@
+# ccp4io/libccp4/fortran/....
+# ccp4_diskio_f.c
+# ccp4_general.c
+# ccp4_general_f.c
+# ccp4_parser_f.c
+# ccp4_program.c
+# ccp4_unitcell_f.c
+# cmaplib_f.c
+# cmtzlib_f.c
+# csymlib_f.c
+# library_f.c
+
+
+function(rewrite_printf source dest)
+  if(${source} IS_NEWER_THAN ${CMAKE_BINARY_DIR}/${dest})
+    message("Writing ${dest}")
+    file(READ ${source} TEXT)
+    # STRING(REGEX REPLACE ";" "\\\;" TEXT "${TEXT}")
+    string(REGEX REPLACE "([^A-Za-z0-9_])printf([^A-Za-z0-9_])" "\\1ccp4io_printf\\2" TEXT "${TEXT}")
+    string(REGEX REPLACE "([^A-Za-z0-9_])fprintf([^A-Za-z0-9_])" "\\1ccp4io_fprintf\\2" TEXT "${TEXT}")
+    string(CONCAT TEXT "#include <ccp4io_adaptbx/printf_wrappers.h>\n" "${TEXT}")
+    file(WRITE ${CMAKE_BINARY_DIR}/${dest} "${TEXT}")
+  endif()
+endfunction()
+
+function(rewrite_csymlib source dest)
+  if(${source} IS_NEWER_THAN ${CMAKE_BINARY_DIR}/${dest})
+    message("Writing ${dest}")
+    file(READ ${source} TEXT)
+    string(REGEX REPLACE "static int reported_syminfo = 0" "static int reported_syminfo = 1" TEXT "${TEXT}")
+    file(WRITE ${CMAKE_BINARY_DIR}/${dest} "${TEXT}")
+  endif()
+endfunction()
+
+# open(op.join(build_ccp4io_adaptbx, "csymlib.c"), "w").write(
+#   open(op.join(path_lib_src, "csymlib.c")).read()
+#     .replace(
+#       "",
+#       ""))
---
+++ cmake/Modules/LibTBXRefresher.cmake
@@ -0,0 +1,33 @@
+
+get_filename_component(LIBTBX_REFRESH_PY ${CMAKE_CURRENT_LIST_DIR}/../run_libtbx_refresh.py ABSOLUTE)
+
+function(add_libtbx_refresh_command refresh_script)
+  # Check that we haven't done this for this project before
+  if (NOT TARGET Python::Python)
+    message(FATAL_ERROR "No python interpreter imported executable Python::Python")
+  endif()
+  if (TARGET ${PROJECT_NAME}_refresh)
+    message(FATAL_ERROR "Asked to create more than one libtbx refresh for the same project ${PROJECT_NAME}")
+  endif()
+  if (NOT TARGET ${PROJECT_NAME})
+    message(FATAL_ERROR "Asked to create libtbx refresh files, but no target named ${PROJECT_NAME}")
+  endif()
+  if (NOT TARGET refresh_meta)
+    add_custom_target(refresh_meta)
+    set_target_properties (refresh_meta PROPERTIES FOLDER meta)
+  endif()
+  # Extract the output parameters from this
+  # cmake_parse_arguments(MY_INSTALL "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+  cmake_parse_arguments(TBXR "" "" "OUTPUT" ${ARGN})
+  # Add the custom command
+  add_custom_command(
+    COMMAND Python::Python ${LIBTBX_REFRESH_PY} --root=${CMAKE_SOURCE_DIR} --output=${CMAKE_BINARY_DIR} ${refresh_script}
+    OUTPUT ${TBXR_OUTPUT} 
+    ${TBXR_UNPARSED_ARGUMENTS})
+  # Make a custom target and then tie the parent target to this
+  add_custom_target(${PROJECT_NAME}_refresh
+    DEPENDS ${TBXR_OUTPUT} )
+  set_target_properties (${PROJECT_NAME}_refresh PROPERTIES FOLDER refresh)
+  add_dependencies(${PROJECT_NAME} ${PROJECT_NAME}_refresh)
+  add_dependencies(refresh_meta ${PROJECT_NAME}_refresh)
+endfunction()
